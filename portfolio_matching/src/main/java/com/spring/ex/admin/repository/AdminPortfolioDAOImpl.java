a25c6594a3a381ed8af0a4f335722967c42ead36
jeongwon201 <jeongwon201@naver.com>
2021-11-25 16:17:36 +0900

    @어어어엉

--- portfolio_matching/src/main/java/com/spring/ex/admin/repository/AdminPortfolioDAOImpl.java
+++ portfolio_matching/src/main/java/com/spring/ex/admin/repository/AdminPortfolioDAOImpl.java
@@ -7,9 +7,11 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.spring.ex.deal.domain.DealVO;
 import com.spring.ex.portfolio.domain.CategoryMainVO;
 import com.spring.ex.portfolio.domain.CategorySubVO;
 import com.spring.ex.portfolio.domain.PortRegVO;
+import com.spring.ex.review.domain.ReviewVO;
 
 @Repository
 public class AdminPortfolioDAOImpl implements AdminPortfolioDAO {
@@ -45,5 +47,40 @@ public class AdminPortfolioDAOImpl implements AdminPortfolioDAO {
 	@Override
 	public int adminPortfolioDelete(int portfolio_id) throws Exception {
 		return sqlSession.delete("adminPortfolioMapper.adminPortfolioDelete", portfolio_id);
+	}
+
+	@Override
+	public PortRegVO adminPortfolioDetail(int portfolio_id) throws Exception {
+		return sqlSession.selectOne("adminPortfolioMapper.adminPortfolioDetail", portfolio_id);
+	}
+
+	@Override
+	public int adminPortfolioAllSaleCount(int portfolio_id) throws Exception {
+		return sqlSession.selectOne("adminPortfolioMapper.adminPortfolioAllSaleCount", portfolio_id);
+	}
+
+	@Override
+	public int adminPortfolioAllSalePrice(int portfolio_id) throws Exception {
+		return sqlSession.selectOne("adminPortfolioMapper.adminPortfolioAllSalePrice", portfolio_id);
+	}
+
+	@Override
+	public int adminPortfolioAllReviewCount(int portfolio_id) throws Exception {
+		return sqlSession.selectOne("adminPortfolioMapper.adminPortfolioAllReviewCount", portfolio_id);
+	}
+
+	@Override
+	public int adminPortfolioAllInquiryCount(int portfolio_id) throws Exception {
+		return sqlSession.selectOne("adminPortfolioMapper.adminPortfolioAllInquiryCount", portfolio_id);
+	}
+
+	@Override
+	public List<DealVO> adminPortfolioRecentDeal(int portfolio_id) throws Exception {
+		return sqlSession.selectList("adminPortfolioMapper.adminPortfolioRecentDeal", portfolio_id);
+	}
+
+	@Override
+	public List<ReviewVO> adminPortfolioRecentReview(int portfolio_id) throws Exception {
+		return sqlSession.selectList("adminPortfolioMapper.adminPortfolioRecentReview", portfolio_id);
 	}
 }
